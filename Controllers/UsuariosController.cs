a83458c9d569326fbd6394ff13b5eb2bf49277a2
Fernando Nicolás <fersanti2896@gmail.com>
2022-11-03 13:18:46 -0600

    Implementando vista de login en UsuariosController.cs

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,4 +1,5 @@
 ﻿using ManejoTareas.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -44,6 +45,39 @@ namespace ManejoTareas.Controllers {
 
                 return View(model);
             }
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login() {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model) {
+            if (!ModelState.IsValid) {
+                return View(model);
+            }
+
+            var resultado = await signInManager.PasswordSignInAsync(model.Email, 
+                                                                    model.Password, 
+                                                                    model.Recordar, 
+                                                                    lockoutOnFailure: false);
+
+            if (resultado.Succeeded) {
+                return RedirectToAction("Index", "Home");
+            } else {
+                ModelState.AddModelError(string.Empty, "Nombre de usuario o contraseña incorrectos.");
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout() { 
+            await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
