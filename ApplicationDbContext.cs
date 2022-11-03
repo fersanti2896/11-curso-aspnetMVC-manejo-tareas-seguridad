abb9ed088dd280ecbe3c06d05f7629b380b019b9
Fernando Nicolás <fersanti2896@gmail.com>
2022-11-03 11:52:21 -0600

    Subiendo proyecto desde S10

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ManejoTareas.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManejoTareas {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions options) : base(options) {
+        }
+
+        /* API FLuent */
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { 
+            base.OnModelCreating(modelBuilder);
+
+            /* modelBuilder.Entity<Tarea>().Property(t => t.Titulo)
+                                        .HasMaxLength(250)
+                                        .IsRequired(); */
+        }
+
+        public DbSet<Tarea> Tareas { get; set; }
+        public DbSet<Paso> Pasos { get; set; }
+        public DbSet<ArchivoAdjunto> ArchivosAdjuntos { get; set; }
+    }
+}
